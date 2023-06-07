6cbdf361f2f2d904f93335d9cff81e576de902a4
Fernanda Novais Avelino <126677085+Fernandaavelino@users.noreply.github.com>
2023-06-07 12:15:12 -0300

    COMMIT

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Criar elemento que irá rodar o jogo
-let canvas = document.getElementById:("snake");
+let canvas = document.getElementById("gamesnake");
 let context = canvas.getContext("2d");
 let box = 32;
 
@@ -7,13 +7,13 @@ let box = 32;
 let snake = [];
 
 //Inicio da cobrinha
-snake[0] = {
+snake[0] ={
     x: 8 * box,
     y: 8 * box
 }
  
 // Direção
-let direction = "right";
+let direction = "right"
 
 // Comida
  let food ={
@@ -23,7 +23,7 @@ let direction = "right";
 
  //Função para criar o Background
  function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightgreen"
 
 //Desenha o retângulo usando x e y e a largura e autura setadas
      context.fillRect(0, 0, 16 * box, 16 * box);
@@ -32,9 +32,15 @@ let direction = "right";
  //Função para criar a cobrinha
    function criarCobrinha (){
       for(i = 0; i < snake.length; i++){
-          context.fillStyle = "green";
-          context.fillRect(snake[i].x, snake[i].y, box, box);
+          context.fillStyle = "green"
+          context.fillRect(snake[i].x, snake[i].y, box, box)
       }
+    }
+
+      //Função para desenhar comida
+      function drawFood() {
+        context.fillStyle = "red"
+        context.fillRect(food.x, food.y, box, box)
       }
 
       //  Quando um evento acontece, detecta e chama a função update
@@ -42,9 +48,9 @@ let direction = "right";
 
       function update(event) {
           if(event.keyCode == 37 && direction != 'right') direction = 'left';
-          if(event.keyCode == 37 && direction != 'right') direction = 'up';
-          if(event.keyCode == 37 && direction != 'right') direction = 'right';
-          if(event.keyCode == 37 && direction != 'right') direction = 'down';
+          if(event.keyCode == 38 && direction != 'down') direction = 'up';
+          if(event.keyCode == 39 && direction != 'left') direction = 'right';
+          if(event.keyCode == 40 && direction != 'up') direction = 'down';
       }
 
       //Função Principal 
@@ -52,15 +58,22 @@ let direction = "right";
         if(snake[0].x > 15*box && direction == "right"){
             snake[0].x = 0;
         }
-        if(snake[0].x < 0 && direction == 'left'){
+        if(snake[0].x < 0 && direction == "left"){
             snake[0].x = 16 * box;
         }
         if(snake[0].y > 15*box && direction == "down"){
             snake[0].y = 0;
         }
-        if(snake[0].y < 0 && direction == 'up'){
+        if(snake[0].y < 0 && direction == "up"){
             snake[0].y = 16 * box;
         }
+         for(i = 1; i < snake.length; i++) {
+            if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+                clearInterval(jogo);
+                alert('Game Over >:O')                
+            }
+         }
+           
         criarBG();
         criarCobrinha();
         drawFood();
@@ -71,17 +84,20 @@ let direction = "right";
         if(direction == "right") snakeX += box;
         if(direction == "left") snakeX -= box;
         if(direction == "up") snakeY -= box;
-        if(direction == "down") snakeY -= box;
+        if(direction == "down") snakeY += box;
 
         if( snakeX != food.x || snaKeY != food.y) {
-            snake.pop(); //pop tira o ultimo elemento da lista
+            //pop tira o ultimo elemento da lista
+            snake.pop();
         }else{
             food.x = Math.floor(Math.random() * 15 +1) * box;
             food.y = Math.floor(Math.random() * 15 +1) * box;
         }
         let newHead ={
             x: snakeX,
-            y: snakeY,
+            y: snakeY
         }
-        snake.unshift(newHead); //método unshift adiciona como primeiro quadradinho da cobrinha
+        //método unshift adiciona como primeiro quadradinho da cobrinha
+        snake.unshift(newHead) 
       }
+      let jogo = setInterval(iniciarJogo, 250);
